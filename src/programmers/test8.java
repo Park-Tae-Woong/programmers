097cd5bdd25b4f003b8351993177f6ba4e692327
Park-Tae_Woong <ptw0428@naver.com>
2022-06-28 02:45:46 +0900

    문제 8번 추가

---
+++ src/programmers/test8.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class test8 {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,3,5,7};
+        int sum = 0;
+        for (int i = 0; i < arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println((double) sum/ arr.length);
+    }
+}
