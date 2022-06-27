f0e6cbe799402cddd9d0ee9b73a9545d7b550293
Park-Tae_Woong <ptw0428@naver.com>
2022-06-28 00:34:42 +0900

    키워드 - 1번 풀이
    변경사항 : 1번 풀이

--- src/programmers/test1.java
+++ src/programmers/test1.java
@@ -1,4 +1,17 @@
 package programmers;
+import java.util.*;
 
 public class test1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        for (int i = 0; i<m; i++){
+            for (int j = 0 ; j<n; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
 }
